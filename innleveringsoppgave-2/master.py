57e9674a903a0f2aa0c18bcc2e20abf60d0086ae
danielnem44 <danielnem@uia.no>
2025-11-24 16:12:39 +0100

    Added comments

---
+++ innleveringsoppgave-2/master.py
@@ -0,0 +1,4 @@
+# master.py
+# Interaktiv historie for innlevering IS-118, basert på Del 1 (Erling og medborgerportalen).
+# Programmet inneholder tre beslutningspunkter med tilhørende valg og tre mulige utfall.
+# Dette er gruppens felles versjon, samlet og kvalitetssikret etter individuelle bidrag.
